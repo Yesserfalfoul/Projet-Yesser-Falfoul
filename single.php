f15f62083a7a4352e9b2ce14a195c797651db7f1
Yesserfalfoul <62869947+Yesserfalfoul@users.noreply.github.com>
2022-05-18 12:22:58 +0100

    Add files via upload

---
+++ single.php
@@ -0,0 +1,142 @@
+<?php include('inc/header.php') ?>
+<?php include('inc/nav.php')?>
+	
+	<div class="close-btn fa fa-times"></div>
+	
+	<!-- SHOP CONTENT -->
+	<section id="content">
+		<div class="content-blog">
+			<div class="container">
+				<div class="row">
+					<div class="page_header text-center">
+						<h2>Shop</h2>
+						<p>Get the best kit for Batata</p>
+					</div>
+
+				
+					<div class="col-md-10 col-md-offset-1">
+
+					<div class="row">
+						<div class="col-md-5">
+							<div class="gal-wrap">
+								<div id="gal-slider" class="flexslider">
+									<ul class="slides">
+										<li><img src="images/shop/1.jpg" class="img-responsive" alt="batata"/></li>
+										<li><img src="images/shop/2.jpg" class="img-responsive" alt="batata"/></li>
+										<li><img src="images/shop/3.jpg" class="img-responsive" alt="batata"/></li>
+										<li><img src="images/shop/4.jpg" class="img-responsive" alt="batata"/></li>
+									</ul>
+								</div>
+								<ul class="gal-nav">
+									<li>
+										<div>
+											<img src="images/shop/1.jpg" class="img-responsive" alt=""/>
+										</div>
+									</li>
+									<li>
+										<div>
+											<img src="images/shop/2.jpg" class="img-responsive" alt=""/>
+										</div>
+									</li>
+									<li>
+										<div>
+											<img src="images/shop/3.jpg" class="img-responsive" alt=""/>
+										</div>
+									</li>
+									<li>
+										<div>
+											<img src="images/shop/4.jpg" class="img-responsive" alt=""/>
+										</div>
+									</li>
+								</ul>
+								<div class="clearfix"></div>
+							
+							</div>
+						</div>
+						<div class="col-md-7 product-single">
+							<h2 class="product-single-title no-margin">batata</h2>
+							<div class="space10"></div>
+							<div class="p-price">40dt</div>
+							<div class="product-quantity">
+								<span>Quantity:</span> 
+								<form>
+									<input type="text" placeholder="1">
+								</form>
+							</div>
+							<div class="shop-btn-wrap">
+								<a href="#" class="button btn-small">Add to Cart</a>
+</div>
+					</div>
+					<div class="clearfix space30"></div>
+					<div class="tab-style3">
+						
+						<div class="align-center mb-40 mb-xs-30">
+							<ul class="nav nav-tabs tpl-minimal-tabs animate">
+								<li class="active col-md-4">
+									<a aria-expanded="true" href="#mini-one" data-toggle="tab">Overview</a>
+								</li>
+								<li class="col-md-4">
+									<a aria-expanded="false" href="#mini-two" data-toggle="tab">Product Info</a>
+								</li>
+								<li class="col-md-4">
+									<a aria-expanded="false" href="#mini-three" data-toggle="tab">Reviews</a>
+								</li>
+							</ul>
+						</div>
+						<div style="height: auto;" class="tab-content tpl-minimal-tabs-cont align-center section-text">
+							<div style="" class="tab-pane fade active in" id="mini-one">
+								<table class="table tba2">
+									<tbody>
+										<tr>
+											<td>Sizes</td>
+											<td>M, L, XL, XXL</td>
+										</tr>
+										<tr>
+											<td>Prodused in</td>
+											<td>Gafsa</td>
+										</tr>
+										<tr>
+											<td>Material</td>
+											<td>plastic, textile</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+							<div style="" class="tab-pane fade" id="mini-two">
+								<table class="table tba2">
+									<tbody>
+										<tr>
+											<td>Sizes</td>
+											<td>M, L, XL, XXL</td>
+										</tr>
+										<tr>
+											<td>Prodused in</td>
+											<td>gabes</td>
+										</tr>
+										<tr>
+											<td>Material</td>
+											<td>plastic, textile</td>
+										</tr>
+										<tr>
+											<td>Colors</td>
+											<td>red, black, grey</td>
+										</tr>
+										<tr>
+											<td>Dimension</td>
+											<td>20x40x33</td>
+										</tr>
+										<tr>
+											<td>Type</td>
+											<td>bag</td>
+										</tr>
+										<tr>
+											<td>Weight</td>
+											<td>0.35kg</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+							
+
+	
+<?php include('inc/footer.php')?>
